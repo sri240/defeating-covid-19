6b524072b132714bcad1f47fc58814df66188de1
art3m1s <anusrisaraf20@gmail.com>
2020-07-29 22:29:29 -0700

    added google form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,6 +14,7 @@ const ContactPage = () => (
     <div class="uuu">
     <h1>have something to share?</h1>
     <div class="contact">
+      <h1 class="jjj">contact me...</h1>
           <form action="mailto:anusrisaraf20@gmail.com" method="post" encType="text/plain">
             name:<br></br>
             <input type="text" name="name" size="30" class="owo"></input><br></br><br></br><br></br>
@@ -25,7 +26,13 @@ const ContactPage = () => (
             <input class="uwu" type="reset" value="clear"></input>
           </form>
           </div>
+
+          <div class="google">
+      <h1 class="jj">...or directly fill out an entry right here!</h1>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdyB8MSGQSqiGnnBYJyvPDhFjLcmKDdViN7GmhYz__6N6YD2w/viewform?embedded=true" width="640" height="888" frameborder="0" marginheight="0" marginwidth="0" style={{marginLeft:"20%"}}>Loading…</iframe>
+    </div>
       </div>
+    
   </Layout>
 )
 
