b7678e1e05b09552c3cb863b7c0272b640f1a722
art3m1s <anusrisaraf20@gmail.com>
2020-08-20 01:20:36 -0700

    changed social card theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#a9ddf5`,
         theme_color: `#a9ddf5`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
